201a54ba089b6cab3ab1e1043277e7a2b833bc13
parrsi01 <parrsi01@luther.edu>
2021-12-02 19:36:30 +0100

    adding files

--- Image and Video Processing With Python/processing.py
+++ Image and Video Processing With Python/processing.py
@@ -3,3 +3,16 @@
 # Title: Image and Video Processing with Python
 
 import cv2
+
+
+img = cv2.imread("galaxy.jpg",0)
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized_image = cv2.resized(img,(1000,500))
+cv2.imshow("Galaxy", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
